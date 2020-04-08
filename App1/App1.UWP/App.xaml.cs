0fdc10c8c5a7395b3b165d85078a8e20a3b1fd56
Kenneth Walker <kewalker@uvic.ca>
2020-04-08 14:19:26 -0700

    Update quickstart with content.

---
+++ App1/App1.UWP/App.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Reflection;
+using Geocortex.Mobile.Platform;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Navigation;
+
+namespace App1.UWP
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public sealed partial class App : Application
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App"/> class.
+        /// The singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+            WindowsAppHandlers.HandleExceptions(this);
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            /* Call any 3rd party library Init methods here */
+
+            // Pass in any 'extra' assemblies, that will be passed along to a Xamarin.Forms Init() call
+            WindowsAppHandlers.HandleOnLaunched(e, typeof(MainPage), Array.Empty<Assembly>());
+        }
+
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            /* Call any 3rd party library Init methods here */
+
+            // Handle Geocortex Mobile startup parameters
+            // Need to add a uri schema declaration in the Package.appxmanifest, then uncomment code below
+            // Pass in any 'extra' assemblies, that will be passed along to a Xamarin.Forms Init() call
+            // WindowsAppHandlers.HandleOnActivated(args, typeof(MainPage), Array.Empty<Assembly>());
+        }
+
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails.
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation.</param>
+        /// <param name="e">Details about the navigation failure.</param>
+        private static void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private static void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+
+            // TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
---
+++ App1/App1/XmlNamespaces.cs
@@ -0,0 +1,7 @@
+﻿namespace App1
+{
+    internal static class XmlNamespaces
+    {
+        public const string App1Namespace = "https://your.org/layout/app1";
+    }
+}
