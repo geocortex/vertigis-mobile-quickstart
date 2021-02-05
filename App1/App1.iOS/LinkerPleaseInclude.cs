c1bb6dd796acbba311aacf887ff8cb967b440cda
Felicity Rhone 🌼 <frhone@latitudegeo.com>
2021-02-05 11:40:06 -0800

    Add a LinkerPleaseInclude file to fix issue with custom MapViewRenderer not being respected

---
+++ App1/App1.iOS/LinkerPleaseInclude.cs
@@ -0,0 +1,9 @@
+﻿using Esri.ArcGISRuntime.Xamarin.Forms.Platform.iOS;
+
+namespace App1.iOS
+{
+    // Get around what seems to be a linker issue, allowing custom MapViewRenderer to be used.
+    class LinkerPleaseInclude : MapViewRenderer
+    {
+    }
+}
